bebe5f4a427d130d1fcd8a7c53f40ebccf278386
James Keveren <james@keve.ren>
2019-09-11 00:13:53 +0200

    suppliers, products and parts in. prices calculated and displayed

--- index.mjs
+++ index.mjs
@@ -1,13 +1,18 @@
 import iterateReplaceProperties from './iterateReplaceProperties.mjs';
 import log from './log.mjs';
+import readline from 'readline';
+
+// suppliers and product details
 
 const squareTubeLink = 'https://hyadelaljarafe.es/hierros/tubos/tubo-cuadrado/';
 const plateLink = 'https://hyadelaljarafe.es/hierros/chapas/chapas-a-medida/';
 
 const suppliers = {
-	'Hierros y Aieros': {
-		link: 'https://hyadelaljarafe.es/',
-		products: {
+	'Hierros y Aieros': [
+		'https://hyadelaljarafe.es/',
+		0.21,
+		45,
+		{
 			squareTube80_2_2500:          [17.68, 1, squareTubeLink],
 			squareTube45_1mm5_2500:       [ 7.83, 1, squareTubeLink],
 			squareTube60_2_2000:          [ 8.18, 1, squareTubeLink],
@@ -15,10 +20,12 @@ const suppliers = {
 			plate_5_1200_625:             [46.00, 1, plateLink],
 			plate_2_2000_1000:            [49.00, 1, plateLink]
 		}
-	},
-	'RS Components Spain': {
-		link: 'https://es.rs-online.com',
-		products: {
+	],
+	'RS Components Spain': [
+		'https://es.rs-online.com',
+		0.21,
+		0,
+		{
 			wireRope_1mm8_100m:         [66.73,   1, 'https://es.rs-online.com/web/p/products/1244748/'],
 			shaft_20_1000:              [40.47,   3, 'https://es.rs-online.com/web/p/varillas-barras-tubos-hexagonales-de-acero/0770440/'],
 			bearing_20_32_7:            [ 3.10,   1, 'https://es.rs-online.com/web/p/rodamientos-de-bola/6190323/'],
@@ -33,21 +40,34 @@ const suppliers = {
 			allenBolt_M8_20:            [16.66,  50, 'https://es.rs-online.com/web/p/tornillos-allen/4838398/'],
 			allenBolt_M8_60:            [31.23,  50, 'https://es.rs-online.com/web/p/tornillos-allen/4839515/'],
 		}
-	}
+	]
 };
 
-// products
+// create full products
 
 const products = {};
 
-for (const [supplierKey, supplier] of Object.entries(suppliers)) {
+for (const [supplierKey, supplierDetails] of Object.entries(suppliers)) {
+	var supplier = suppliers[supplierKey] = {
+		link: supplierDetails[0],
+		VATRate: supplierDetails[1],
+		shippingPrice: supplierDetails[2],
+		products: supplierDetails[3],
+		totalProductCost: 0,
+		orderVAT: 0,
+	}
 	for (let [productKey, productDetails] of Object.entries(supplier.products)) {
 		products[productKey] = supplier.products[productKey] = {
+			key: productKey,
 			supplier,
 			packPrice: productDetails[0],
 			packSize: productDetails[1],
 			link: productDetails[2],
 			parts: [],
+			requiredItemCount: 0,
+			requiredPackCount: 0,
+			spareCount: 0,
+			totalCost: 0,
 		};
 	}
 }
@@ -73,9 +93,9 @@ const parts = {
 	legGuideSupports:                              [p.squareTube45_1mm5_2500, legCount],
 	legs:                                          [p.squareTube60_2_2000, legCount],
 	surfaceStructure:                              [surfaceStructureProduct, surfaceStructureMemberCount],
-	surfaceStructureEnds:                          surfaceStructureProduct,
+	surfaceStructureEnds:                          surfaceStructureProduct, // cut for 2 pieces
 	surface:                                       [p.plate_5_1200_625, surfacePlateCount],
-	hoistRope:                                     p.wireRope_1mm8_100m,
+	hoistRope:                                     p.wireRope_1mm8_100m, // cut for 4 pieces
 	pulleyBearings:                                [p.bearing_20_32_7, 8.5 * legCount], // 4 at top, 2 on bottom outside, 1 bottom inside, 1(2 on half of legs) for directing cable to spool
 	shaft:                                         [p.shaft_20_1000, 2],
 	shaftWashers:                                  [p.washer_M8_25_1mm5, shaftFastenerCount], // goes on the bolts not the shaft itself
@@ -100,6 +120,8 @@ const parts = {
 	legGuideSupportToSurfaceStructureBolts:        [p.allenBolt_M8_60, legGuideSupportToSurfaceStructureFastenerCount],
 };
 
+// validate, create full parts and link parts to products
+
 for (const [partKey, partDetails] of Object.entries(parts)) {
 	const partDetailsIsArray = partDetails instanceof Array;
 	const product = partDetailsIsArray ? partDetails[0] : partDetails
@@ -114,3 +136,109 @@ for (const [partKey, partDetails] of Object.entries(parts)) {
 		count: partDetailsIsArray ? partDetails[1] || 1 : 1,
 	});
 }
+
+// count number of individual items required (i.e. bolts not bags of bolts)
+for (const product of Object.values(products)) {
+	for (const part of product.parts) {
+		product.requiredItemCount += part.count;
+	}
+}
+
+// calculate number of packs required and cost for packs
+for (const product of Object.values(products)) {
+	product.requiredPackCount = Math.ceil(product.requiredItemCount / product.packSize);
+	product.spareCount = product.requiredPackCount * product.packSize - product.requiredItemCount;
+	product.totalCost = product.packPrice * product.requiredPackCount
+}
+
+// calculate total cost of order from each supplier
+for (const supplier of Object.values(suppliers)) {
+	for (const product of Object.values(supplier.products)) {
+		supplier.totalProductCost += product.totalCost;
+	}
+	supplier.orderVAT = (supplier.totalProductCost + supplier.shippingPrice) * supplier.VATRate;
+	supplier.orderTotal = supplier.totalProductCost + supplier.shippingPrice + supplier.orderVAT;
+}
+
+// display results
+
+const moveCursor = (x = 0) => readline.cursorTo(process.stdout, x);
+const write = (...values) => {
+	for (const value of values) {
+		process.stdout.write(value.toString());
+	}
+}
+
+const indentation = 2;
+
+let cumulativeColumnPosition = indentation;
+
+const columns = [
+	['Product Key', 'key'],
+	['Count', 'requiredItemCount'],
+	['Pack Size', 'packSize'],
+	['Packs', 'requiredPackCount'],
+	['Pack Price', 'packPrice'],
+	['Total Price', 'totalCost']
+].map((columnDetails, index) => {
+	const column = {
+		title: columnDetails[0],
+		key: columnDetails[1],
+		position: cumulativeColumnPosition
+	}
+	cumulativeColumnPosition += Math.max(column.title.length, ...Object.values(products).map(product => product[column.key].toString().length)) + 1;
+	return column;
+});
+
+const summaryRows = [
+	['Shipping', 'shippingPrice'],
+	['VAT', 'orderVAT'],
+	['Order Total', 'orderTotal']
+];
+
+const summaryValueIndentation = Math.max(...summaryRows.map(summaryRow => summaryRow[0].length)) + indentation + 2;
+
+for (const [supplierName, supplier] of Object.entries(suppliers)) {
+	moveCursor();
+	write('\n', supplierName, ':\n');
+	write('\x1b[4;37m');
+	for (const column of columns) {
+		moveCursor(column.position);
+		write(column.title);
+	}
+	write('\x1b[0m');
+	write('\n');
+	const supplierProductEntries = Object.entries(supplier.products);
+	for (const productIndex in supplierProductEntries) {
+		const [productKey, product] = supplierProductEntries[productIndex];
+		const even = !(productIndex % 2);
+		if (even) {
+			write('\x1b[0;36m');
+		}
+		for (const column of columns) {
+			moveCursor(column.position);
+			write(product[column.key]);
+		}
+		if (even) {
+			write('\x1b[0m');
+		}
+		write('\n');
+	}
+	const lastColumnPosition = columns[columns.length - 1].position;
+	moveCursor(lastColumnPosition - 7);
+	write('Total:')
+	moveCursor(lastColumnPosition);
+	write(supplier.totalProductCost.toFixed(2), '\n');
+	for (const summaryRow of summaryRows) {
+		moveCursor(indentation);
+		write(summaryRow[0], ':');
+		moveCursor(summaryValueIndentation);
+		write(supplier[summaryRow[1]].toFixed(2), '\n');
+	}
+}
+
+write('\n');
+moveCursor(indentation);
+write('Total Cost:');
+moveCursor(summaryValueIndentation);
+write(Object.values(iterateReplaceProperties(suppliers, supplier => supplier.orderTotal)).reduce((sum, current) => sum + current).toFixed(2), '\n\n');
