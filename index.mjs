cb183524430df96a425393fb177bc1e2d06ae878
James Keveren <james@keve.ren>
2020-09-29 20:45:27 +0200

    so many changes

--- index.mjs
+++ index.mjs
@@ -11,14 +11,15 @@ const suppliers = {
 	'Hierros y Aieros': [
 		'https://hyadelaljarafe.es/',
 		0.21,
-		45,
+		60,
 		{
-			squareTube80_2_2500:          [17.68, 1, squareTubeLink],
-			squareTube45_1mm5_2500:       [ 7.83, 1, squareTubeLink],
-			squareTube60_2_2000:          [ 8.18, 1, squareTubeLink],
+			squareTube_80_2_2500:         [17.68, 1, squareTubeLink],
+			squareTube_40_2_2500:         [ 8.13, 1, squareTubeLink],
+			squareTube_60_2_2000:         [ 9.92, 1, squareTubeLink],
 			rectangularTube_60_40_3_2500: [13.26, 1, 'https://hyadelaljarafe.es/hierros/tubos/tubo-rectangular/'],
 			plate_5_1200_625:             [46.00, 1, plateLink],
-			plate_2_2000_1000:            [49.00, 1, plateLink]
+			plate_2_1200_625:             [19.00, 1, plateLink],
+			roundTube_40_1mm5_2500:       [ 5.59, 1, 'https://hyadelaljarafe.es/hierros/tubos/tubo-redondo/'],
 		}
 	],
 	'RS Components Spain': [
@@ -26,13 +27,12 @@ const suppliers = {
 		0.21,
 		0,
 		{
-			wireRope_1mm8_100m:         [66.73,   1, 'https://es.rs-online.com/web/p/products/1244748/'],
+			wireRope_100m_1mm8_2mm4:    [66.73,   1, 'https://es.rs-online.com/web/p/products/1244748/'],
 			shaft_20_1000:              [40.47,   3, 'https://es.rs-online.com/web/p/varillas-barras-tubos-hexagonales-de-acero/0770440/'],
+			bearing_45_75_16:           [10.66,   1, 'https://es.rs-online.com/web/p/rodamientos-de-bola/6190581/'],
 			bearing_20_32_7:            [ 3.10,   1, 'https://es.rs-online.com/web/p/rodamientos-de-bola/6190323/'],
-			castorWheel:                [16.40,   1, 'https://es.rs-online.com/web/p/ruedas-industriales/7871402/'],
 			threadedRivet_M8:           [19.81, 100, 'https://es.rs-online.com/web/p/remaches/7689984/'],
 			flangedNut_M8_8:            [11.15, 100, 'https://es.rs-online.com/web/p/tuercas-hexagonales-con-reborde/0275484/'],
-			lockNut_M8_8:               [ 9.67, 100, 'https://es.rs-online.com/web/p/tuercas-de-bloqueo/1224373/'],
 			washer_M8_17_1mm6:          [ 3.61, 100, 'https://es.rs-online.com/web/p/arandelas-planas/0527634/'],
 			washer_M8_25_1mm5:          [ 6.35, 100, 'https://es.rs-online.com/web/p/arandelas-guardabarros/6667772/'],
 			countersunkAllenBolt_M8_20: [13.50,  50, 'https://es.rs-online.com/web/p/tornillos-allen/0281552/'],
@@ -79,35 +79,41 @@ const p = products;
 
 const legCount = 4; // bacause magic numbers cause confusion
 const surfaceStructureProduct = p.rectangularTube_60_40_3_2500;
-const surfacePlateCount = 5;
 const surfaceStructureMemberCount = 7;
-const surfaceToSurfaceStructureFastenerCount = 2 * surfacePlateCount * surfaceStructureMemberCount;
-const surfaceStructureToSurfaceStructureEndsFastenerCount = (surfaceStructureMemberCount - 2) * 4;
-const shaftFastenerCount = (8.5) * legCount; // 2 at top, 2 at bottom outside, 1 bottom inside, 1(2 on half of legs) for directing cable to spool
-const lowerShaftSupportFastenerCount = 4 * legCount;
-const surfaceStructureToLegGuideFastenerCount = (2 * 2) * legCount; // 2 per joint, 2 joints per leg
+const surfacePlateCount = 4;
+const hoistRopeFastenerCount = 8 * legCount;
+const surfaceToSurfaceStructureFastenerCount = 4 * surfacePlateCount * surfaceStructureMemberCount;
+const surfaceStructureToSurfaceStructureEndsFastenerCount = (surfaceStructureMemberCount - 2) * 8;
+const pulleyShaftFastenerCount = (8.5) * legCount; // 2 at top, 2 at bottom outside, 1 bottom inside, 1(2 on half of legs) for directing cable to spool
+// const driveShaftbearingToSurfaceStructureFastenerCount =
+const lowerpulleyShaftSupportFastenerCount = 4 * legCount;
+const surfaceStructureToLegGuideFastenerCount = (6 * 2) * legCount; // 4 per joint, 2 joints per leg
 const legGuideSupportToLegGuideFastenerCount = (2 * 2) * legCount; // 2 per joint, 2 joints per leg
-const legGuideSupportToSurfaceStructureFastenerCount = 2 * legCount;
+const legGuideSupportToSurfaceStructureFastenerCount = 4 * legCount;
+const wheelToWheelMountFastenerCount = 4 * legCount;
+const wheelMountToLegFastenerCount = 2 * legCount;
 
 const parts = {
-	legGuides:                                     [p.squareTube80_2_2500, 1],
-	legGuideSupports:                              [p.squareTube45_1mm5_2500, legCount],
-	legs:                                          [p.squareTube60_2_2000, legCount],
+	legGuides:                                     [p.squareTube_80_2_2500, 1],
+	legGuideSupports:                              [p.squareTube_40_2_2500, legCount],
+	legs:                                          [p.squareTube_60_2_2000, legCount],
 	surfaceStructure:                              [surfaceStructureProduct, surfaceStructureMemberCount],
 	surfaceStructureEnds:                          surfaceStructureProduct, // cut for 2 pieces
 	surface:                                       [p.plate_5_1200_625, surfacePlateCount],
-	hoistRope:                                     p.wireRope_1mm8_100m, // cut for 4 pieces
+	hoistRope:                                     p.wireRope_100m_1mm8_2mm4, // cut for 4 pieces
+	hoistRopeNuts:                                 [p.flangedNut_M8_8, hoistRopeFastenerCount],
+	hoistRopeRetensionPlates:                      [p.plate_2_1200_625, 0.1],
+	hoistRopeBolt:                                 [p.allenBolt_M8_12, hoistRopeFastenerCount],
 	pulleyBearings:                                [p.bearing_20_32_7, 8.5 * legCount], // 4 at top, 2 on bottom outside, 1 bottom inside, 1(2 on half of legs) for directing cable to spool
-	shaft:                                         [p.shaft_20_1000, 2],
-	wheel:                                         [p.castorWheel, legCount],
-	// add wheels here
-	// nuts, washers and bolts:
-	shaftWashers:                                  [p.washer_M8_25_1mm5, shaftFastenerCount], // goes on the bolts not the shaft itself
-	shaftBolts:                                    [p.allenBolt_M8_20, shaftFastenerCount],
-	lowerShaftSupport:                             [p.plate_2_2000_1000, 0.1], // dont need an entire sheet but order one because its useful
-	lowerShaftSupportNut:                          [p.flangedNut_M8_8, lowerShaftSupportFastenerCount], // outside 2 pulleys and inside pulley
-	lowerShaftSupportWasher:                       [p.washer_M8_17_1mm6, lowerShaftSupportFastenerCount],
-	lowerShaftSupportBolt:                         [p.allenBolt_M8_16, lowerShaftSupportFastenerCount], // use 16mm bolts but cut the ends off
+	pulleyShaft:                                   [p.shaft_20_1000, 2],
+	pulleyShaftWashers:                            [p.washer_M8_25_1mm5, pulleyShaftFastenerCount], // goes on the bolts not the pulleyShaft itself
+	pulleyShaftBolts:                              [p.allenBolt_M8_20, pulleyShaftFastenerCount],
+	driveShaft:                                    [p.roundTube_40_1mm5_2500, 1],
+	driveShaftBearings:                            [p.bearing_45_75_16, 4],
+	lowerpulleyShaftSupport:                       [p.plate_2_1200_625, 0.1], // dont need an entire sheet but order one because its useful
+	lowerpulleyShaftSupportNut:                    [p.flangedNut_M8_8, lowerpulleyShaftSupportFastenerCount], // outside 2 pulleys and inside pulley
+	lowerpulleyShaftSupportWasher:                 [p.washer_M8_17_1mm6, lowerpulleyShaftSupportFastenerCount],
+	lowerpulleyShaftSupportBolt:                   [p.allenBolt_M8_16, lowerpulleyShaftSupportFastenerCount], // use 16mm bolts but cut the ends off
 	surfaceToSurfaceStructureNuts:                 [p.threadedRivet_M8, surfaceToSurfaceStructureFastenerCount],
 	surfaceToSurfaceStructureBolts:                [p.countersunkAllenBolt_M8_20, surfaceToSurfaceStructureFastenerCount],
 	surfaceStructureToSurfaceStructureEndsNuts:    [p.threadedRivet_M8, surfaceStructureToSurfaceStructureEndsFastenerCount],
@@ -119,8 +125,8 @@ const parts = {
 	legGuideSupportToLegGuideNuts:                 [p.flangedNut_M8_8, legGuideSupportToLegGuideFastenerCount],
 	legGuideSupportToLegGuideWashers:              [p.washer_M8_17_1mm6, legGuideSupportToLegGuideFastenerCount],
 	legGuideSupportToLegGuideBolts:                [p.allenBolt_M8_12, legGuideSupportToLegGuideFastenerCount],
-	legGuideSupportToSurfaceStructureNuts:         [p.lockNut_M8_8, legGuideSupportToSurfaceStructureFastenerCount],
-	legGuideSupportToSurfaceStructureWashers:      [p.washer_M8_17_1mm6, legGuideSupportToSurfaceStructureFastenerCount * 2], // washer under both bolt head and nut side
+	legGuideSupportToSurfaceStructureNuts:         [p.flangedNut_M8_8, legGuideSupportToSurfaceStructureFastenerCount],
+	legGuideSupportToSurfaceStructureWashers:      [p.washer_M8_17_1mm6, legGuideSupportToSurfaceStructureFastenerCount], // washer under both bolt head and nut
 	legGuideSupportToSurfaceStructureBolts:        [p.allenBolt_M8_60, legGuideSupportToSurfaceStructureFastenerCount],
 };
 
@@ -130,7 +136,7 @@ for (const [partKey, partDetails] of Object.entries(parts)) {
 	const partDetailsIsArray = partDetails instanceof Array;
 	const product = partDetailsIsArray ? partDetails[0] : partDetails
 	if (!product) {
-		throw new Error(`product for part "${partKey}" does not exist or in unspcified`)
+		throw new Error(`product for part "${partKey}" does not exist or is unspcified`)
 	}
 	if (partDetailsIsArray && partDetails[1] !== undefined && (typeof partDetails[1] !== 'number' || partDetails[1] <= 0)) {
 		throw new Error(`"${partDetails[1]}" is an invalid value for count of part "${partKey}"`);
@@ -183,7 +189,8 @@ const columns = [
 	['Pack Size', 'packSize'],
 	['Packs', 'requiredPackCount'],
 	['Pack Price', 'packPrice'],
-	['Total Price', 'totalCost']
+	['Total Price', 'totalCost'],
+	['Link', 'link']
 ].map((columnDetails, index) => {
 	const column = {
 		title: columnDetails[0],
@@ -228,9 +235,10 @@ for (const [supplierName, supplier] of Object.entries(suppliers)) {
 		}
 		write('\n');
 	}
-	const lastColumnPosition = columns[columns.length - 1].position;
-	moveCursor(lastColumnPosition - 7);
-	write('Total:')
+	const lastColumnPosition = columns[5].position;
+	const totalString = 'Total: '
+	moveCursor(lastColumnPosition - totalString.length);
+	write(totalString);
 	moveCursor(lastColumnPosition);
 	write(supplier.totalProductCost.toFixed(2), '\n');
 	for (const summaryRow of summaryRows) {
